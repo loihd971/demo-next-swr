43d22b2c5b2145f7394ccc14d9c827bede2d11a5
cheer971 <loihd971@gmail.com>
2023-03-03 02:50:52 +0700

    update-code

--- libs/httpClient.ts
+++ libs/httpClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const httpClient = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "http://localhost:3000/api",
 });
 
 // Add a request interceptor
